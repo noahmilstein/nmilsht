422159288fd1fe80279e1a485aadd33cf15c9b94
noahmilstein <casanoah@gmail.com>
2016-12-08 23:56:37 -0500

    added devise and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create, :destroy]
   end

4255e75dbb5ee8c204fc63efc2698c8f83e5c6c4
noahmilstein <casanoah@gmail.com>
2016-12-08 22:19:43 -0500

    edit and delete functioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- resources :posts, only: [:index, :new, :create, :show]
+ resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
  root "posts#index"
 end

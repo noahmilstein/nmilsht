9600b938d8c2cfd37828fc543549ae428738ea78
noahmilstein <casanoah@gmail.com>
2017-01-16 21:28:15 -0500

    updated tests, fixed coveralls badge on readme

--- spec/features/02_posts_spec.rb
+++ spec/features/02_posts_spec.rb
@@ -9,6 +9,7 @@ feature 'blog posts', %Q{
   let!(:user) { FactoryGirl.create(:user, email: ENV['ADMIN']) }
   let!(:post1) { FactoryGirl.create(:post) }
   let(:post2) { Post.new(title: 'testing posts title', body: 'testing posts body, minimum 20 chars') }
+  let(:post3) { Post.new(title: 'testing posts title number 3', body: 'testing posts body number 3, minimum 20 chars') }
 
   before :each do
     fill_sign_in_form(user)
@@ -35,5 +36,15 @@ feature 'blog posts', %Q{
 
     expect(page).to have_content(post2.title)
     expect(page).to have_content(post2.body)
+  end
+
+  scenario 'search for post title' do
+    fill_post_form(post3)
+    fill_in "search", with: post3.title
+    click_button("Search")
+
+    expect(page).to_not have_content(post1.title)
+    expect(page).to_not have_content(post2.title)
+    expect(page).to have_content(post3.title)
   end
 end
